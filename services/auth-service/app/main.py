a3036b4745ac683331396cacbd246c4bb991e6ee
tejasvi102 <141488064+tejasvi102@users.noreply.github.com>
2026-02-08 12:13:10 +0530

    add user service with profile APIs and JWT integration

--- services/auth-service/app/main.py
+++ services/auth-service/app/main.py
@@ -22,7 +22,7 @@ def auth():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "app.main:app",
         host="0.0.0.0",
         port=APP_PORT,
         reload=APP_ENV == "development"
--- services/user-service/app/core/config.py
+++ services/user-service/app/core/config.py
@@ -1,13 +1,19 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
+    # App
     app_name: str
     app_env: str
     app_port: int
 
+    # Database
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
     class Config:
         env_file = ".env"
-
 
 settings = Settings()
